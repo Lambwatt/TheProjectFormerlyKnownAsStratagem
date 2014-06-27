8243e0a62eaeb55d36d12a249f110ef70426274d
Michael Pattie <mjpattie@gmail.com>
2014-06-26 21:52:01 -0700

    started basic files.

--- Tile.js
+++ Tile.js
@@ -18,7 +18,73 @@ function makeUnit(){
 }
 
 function getTile(x, y){
-	return this.tiles[x][y];
+	if(x>0 && x<this.tiles.length && y>0 && y<this.tiles.height)
+		return this.tiles[x][y];
+	else
+		return false;
+}
+
+function getTileInDirection(base_x, base_y, direction){
+	var coords = {x:base_x, y:base_y};
+
+	if(!this.getTile(coords.x, coords.y)){
+		console.log("WARNING: recieved invalid start coordinates ["+base_x+", "+base_y+"]")
+		return false;//
+	}
+
+	coords = getForwardStepInDirection(direction)(coords);
+
+	if(!this.getTile(coords.x, coords.y))
+		coords = getBackwardStepInDirection(direction)(coords);
+
+	return this.getTile(coords.x, coords.y);
+	
+}
+
+function getForwardStepInDirection(direction){
+	switch(direction){
+		case N:
+			return function(coords){return{x:coords, y:coords-1}};
+		case NE:
+			return function(coords){return{x:coords+1, y:coords-1}};
+		case E:
+			return function(coords){return{x:coords+1, y:coords}};
+		case SE:
+			return function(coords){return{x:coords+1, y:coords+1}};
+		case S:
+			return function(coords){return{x:coords, y:coords+1}};
+		case SW:
+			return function(coords){return{x:coords-1, y:coords+1}};
+		case W:
+			return function(coords){return{x:coords-1, y:coords}};
+		case NW:
+			return function(coords){return{x:coords-1, y:coords-1}};
+		default:
+			return function(coords){return{x:coords, y:coords}};
+	}
+}
+
+function getBackwardStepInDirection(direction){
+	switch(direction){
+		case N:
+			return function(coords){return{x:coords, y:coords+1}};
+		case NE:
+			return function(coords){return{x:coords-1, y:coords+1}};
+		case E:
+			return function(coords){return{x:coords-1, y:coords}};
+		case SE:
+			return function(coords){return{x:coords-1, y:coords-1}};
+		case S:
+			return function(coords){return{x:coords, y:coords-1}};
+		case SW:
+			return function(coords){return{x:coords+1, y:coords-1}};
+		case W:
+			return function(coords){return{x:coords+1, y:coords}};
+		case NW:
+			return function(coords){return{x:coords+1, y:coords+1}};
+		default:
+			return function(coords){return{x:coords, y:coords}};
+	}
 }
 
 function makeMap(x, y){
@@ -31,5 +97,8 @@ function makeMap(x, y){
 	}
 
 	map.getTile = getTile;
+	map.getTileInDirection = getTileInDirection;
+
+
 	return map;
 }
